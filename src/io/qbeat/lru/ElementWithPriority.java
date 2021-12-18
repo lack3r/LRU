963d1c07cd5e1543e9e8dd7417bf51aec80edbfc
lack3r <andreasl.loizou@gmail.com>
2021-12-18 15:39:32 +0200

    Added a new LRU that handles priorities as well

---
+++ src/io/qbeat/lru/ElementWithPriority.java
@@ -0,0 +1,39 @@
+package io.qbeat.lru;
+
+public class ElementWithPriority {
+    private String key;
+    private int value;
+
+    private int priority;
+
+    ElementWithPriority(String key, int value, int priority) {
+        this.key = key;
+        this.value = value;
+        this.priority = priority;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void updateValue(int value){
+        this.value = value;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + key + ", " + value + ", " + priority + '}';
+    }
+}
---
+++ src/io/qbeat/lru/LRUCacheWithPriorities.java
@@ -0,0 +1,153 @@
+package io.qbeat.lru;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class LRUCacheWithPriorities {
+
+    private int capacity;
+
+    /**
+     * An implementation of the Last Recently Used (LRU) Cache
+     * Both Set and Get methods require a constant time
+     * Does not support concurrency or expiry date.
+     *
+     * @param capacity The capacity the Cache should have
+     */
+    public LRUCacheWithPriorities(int capacity) {
+        this.capacity = capacity;
+    }
+
+    // We need this, in order to be able to find an element in our cache in constant O(1) time
+    private HashMap<String, DoubleLinkedListNode<ElementWithPriority>> hashmapWithNodes = new HashMap<>();
+    // We need this, for the ordering of our list. The DoubleLinkedList is the perfect structure since we could:
+    // Add an element on the top or tail in constant time.
+    // Move an element to the top of the list, given that we have the node, again in constant time
+//    private DoubleLinkedList<Element> orderedCache = new DoubleLinkedList<>();
+
+    private TreeMap<Integer, DoubleLinkedList<ElementWithPriority>> prioritiesMap = new TreeMap<>();
+
+    // Time complexity: O(1)
+    public Integer get(String key) {
+        DoubleLinkedListNode<ElementWithPriority> node;
+        // O(1)
+        if (hashmapWithNodes.containsKey(key)) {
+            node = hashmapWithNodes.get(key);
+        } else {
+            return null;
+        }
+
+        // O(1)
+        moveToTheTopInPriorityList(node);
+        return node.getElement().getValue();
+    }
+
+    private void moveToTheTopInPriorityList(DoubleLinkedListNode<ElementWithPriority> node) {
+        int priority = node.getElement().getPriority();
+        DoubleLinkedList<ElementWithPriority> cacheForPriority = prioritiesMap.get(priority);
+        cacheForPriority.moveToTheTop(node);
+    }
+
+    // Time complexity: O(1)
+    public void set(String key, int value, int priority) {
+        if (hashmapWithNodes.containsKey(key)) {
+            moveToTheTop(key, value, priority);
+        } else {
+            if (size() == capacity) {
+                // Drop element to make capacity
+                removeLast();
+            }
+            insertToTheTop(key, value, priority);
+        }
+    }
+
+    private void moveToTheTop(String key, Integer value, int updatedPriority) {
+        DoubleLinkedListNode<ElementWithPriority> elementDoubleLinkedListNode = hashmapWithNodes.get(key);
+        int existingPriority = elementDoubleLinkedListNode.getElement().getPriority();
+
+        elementDoubleLinkedListNode.getElement().updateValue(value);
+        elementDoubleLinkedListNode.getElement().setPriority(updatedPriority);
+
+        if (existingPriority != updatedPriority) {
+            // Remove element from list
+            DoubleLinkedList<ElementWithPriority> cacheForPriority = prioritiesMap.get(existingPriority);
+            cacheForPriority.remove(elementDoubleLinkedListNode);
+            if (cacheForPriority.isEmpty()) {
+                prioritiesMap.remove(existingPriority);
+            }
+            if (!prioritiesMap.containsKey(updatedPriority)) {
+                prioritiesMap.put(updatedPriority, new DoubleLinkedList<>());
+            }
+            DoubleLinkedList<ElementWithPriority> cacheForUpdatedPriority = prioritiesMap.get(updatedPriority);
+            elementDoubleLinkedListNode = cacheForUpdatedPriority.putFirst(elementDoubleLinkedListNode.getElement());
+        } else {
+            DoubleLinkedList<ElementWithPriority> cacheForUpdatedPriority = prioritiesMap.get(updatedPriority);
+            cacheForUpdatedPriority.moveToTheTop(elementDoubleLinkedListNode);
+        }
+
+        hashmapWithNodes.put(key, elementDoubleLinkedListNode);
+    }
+
+    public int size() {
+        return hashmapWithNodes.size();
+    }
+
+    private void removeLast() {
+        DoubleLinkedList<ElementWithPriority> cacheForFirstPriority = prioritiesMap.get(prioritiesMap.firstKey());
+        ElementWithPriority elementWithPriority = cacheForFirstPriority.removeLast();
+        if (cacheForFirstPriority.isEmpty()) {
+            prioritiesMap.remove(prioritiesMap.firstKey());
+        }
+        hashmapWithNodes.remove(elementWithPriority);
+    }
+
+    private void insertToTheTop(String key, Integer value, int priority) {
+        if (!prioritiesMap.containsKey(priority)) {
+            prioritiesMap.put(priority, new DoubleLinkedList<>());
+        }
+
+        DoubleLinkedList<ElementWithPriority> cacheForUpdatedPriority = prioritiesMap.get(priority);
+        DoubleLinkedListNode<ElementWithPriority> node = cacheForUpdatedPriority.putFirst(new ElementWithPriority(key, value, priority));
+
+        hashmapWithNodes.put(key, node);
+    }
+
+    @Override
+    public String toString() {
+        if (prioritiesMap.size() == 0) {
+            return "Cache is empty";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Integer, DoubleLinkedList<ElementWithPriority>> entry : prioritiesMap.entrySet()) {
+            sb.append("Priority " + entry.getKey() + ": ");
+            sb.append(entry.getValue().toString());
+            sb.append(System.getProperty("line.separator"));
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LRUCacheWithPriorities lruCacheWithPriorities = new LRUCacheWithPriorities(5);
+        System.out.println(lruCacheWithPriorities);
+        lruCacheWithPriorities.set("A", 5, 4);
+        System.out.println(lruCacheWithPriorities);
+        lruCacheWithPriorities.set("B", 4, 1);
+        System.out.println(lruCacheWithPriorities);
+        lruCacheWithPriorities.set("C", 3, 7);
+        System.out.println(lruCacheWithPriorities);
+        lruCacheWithPriorities.set("D", 2, 8);
+        System.out.println(lruCacheWithPriorities);
+        lruCacheWithPriorities.set("E", 1, 5);
+        System.out.println(lruCacheWithPriorities);
+        lruCacheWithPriorities.get("C");
+        System.out.println(lruCacheWithPriorities);
+        lruCacheWithPriorities.set("F", 4, 5);
+        System.out.println(lruCacheWithPriorities);
+        lruCacheWithPriorities.set("E", 2, 5);
+        System.out.println(lruCacheWithPriorities);
+    }
+}
